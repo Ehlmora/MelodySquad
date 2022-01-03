e973b4c9130f287311f5ca78c80959f8cabefc5a
Olivier Da Pozzo <olivier.dapozzo@live.fr>
2022-01-03 23:43:17 +0100

    Added routing with multiple arguments

--- Controller/router.php
+++ Controller/router.php
@@ -4,15 +4,18 @@ require_once '../Controller/FormationController.php';
 require_once '../Controller/CourseController.php';
 require_once '../Controller/AccountController.php';
 
-switch($_GET['route']) {
+define("ROUTE", explode("/", $_GET['route']));
+
+switch(ROUTE[0]) {
 
     case ''             : include_once '../View/Home/index.php'; break;
     case 'formations'   : FormationController::index(); break;
     case 'courses'      : CourseController::index(); break;
+    case 'login'        : include_once '../View/Account/login.php'; break;
     case 'connect'      : AccountController::connect(); break;
     case 'disconnect'   : AccountController::disconnect(); break;
-    case 'signin'       :
-    case 'dashboard'    :
+    //case 'signin'       :
+    case 'courses-taken': AccountController::dashboard(); break;
     default             : include_once '../View/Error/404.php'; break;
 
 }
