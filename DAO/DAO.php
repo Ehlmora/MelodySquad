12fbc1194b73c762d3b184044acc11fdb6e3c65a
Olivier Da Pozzo <olivier.dapozzo@live.fr>
2022-01-03 18:40:41 +0100

    Add DB connection to DAO

--- DAO/DAO.php
+++ DAO/DAO.php
@@ -1,6 +1,25 @@
 <?php
 
-class MySQLFactory
+class DAO
 {
+    protected $connection;
+
+    public function __construct() {
+        $this->connection = null;
+    }
+
+    public function connect() {
+
+        try {
+
+            $this->connection = new PDO("mysql:host=" . DB_HOST . ";"."dbname=" . DB_NAME, DB_USER, DB_PASS);
+
+        } catch (PDOException $e) {
+
+            print '<div class="alert alert-danger" role="alert">[Erreur]: ' . $e->getMessage() . '<div/>';
+            die();
+        }
+
+    }
 
 }
