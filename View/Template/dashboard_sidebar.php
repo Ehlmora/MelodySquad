9b239048541ceed17911efe5a72ff2af7bbf855f
Olivier Da Pozzo <olivier.dapozzo@live.fr>
2022-01-04 11:12:30 +0100

    Updated dashboard sidebar's routes

--- View/Template/dashboard_sidebar.php
+++ View/Template/dashboard_sidebar.php
@@ -1,16 +1,16 @@
 <nav class="d-flex flex-column flex-shrink-0 bg-white border ms-rounded m-3 px-3 py-5 position-sticky">
     <ul class="nav nav-pills flex-column mb-auto">
         <li class="nav-item">
-            <a href="courses-taken" class="nav-link <?php echo ROUTE[0] == "courses-taken" ? "active" : ""; ?>">Cours suivis</a>
+            <a href="courses-taken" class="nav-link <?php echo ROUTE[1] == "courses-taken" ? "active" : ""; ?>">Cours suivis</a>
         </li>
         <li class="nav-item">
-            <a href="my-writing" class="nav-link <?php echo ROUTE[0] == "my-writing" ? "active" : ""; ?>">Mes rédactions</a>
+            <a href="courses" class="nav-link <?php echo ROUTE[1] == "courses" ? "active" : ""; ?>">Mes rédactions</a>
         </li>
         <li class="nav-item">
-            <a href="formations-management" class="nav-link <?php echo ROUTE[0] == "formations-management" ? "active" : ""; ?>">Gestion des formations</a>
+            <a href="formations" class="nav-link <?php echo ROUTE[1] == "formations" ? "active" : ""; ?>">Gestion des formations</a>
         </li>
-        <li class="accounts-management">
-            <a href="#" class="nav-link <?php echo ROUTE[0] == "accounts-management" ? "active" : ""; ?>">Gestion des comptes</a>
+        <li class="nav-item">
+            <a href="accounts" class="nav-link <?php echo ROUTE[1] == "accounts" ? "active" : ""; ?>">Gestion des comptes</a>
         </li>
     </ul>
 </nav>
