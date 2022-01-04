8f91552240b6a118f7b7a380d7703bb5b4ab8b38
Olivier Da Pozzo <olivier.dapozzo@live.fr>
2022-01-04 11:10:41 +0100

    Added dashboard routes

--- Controller/router.php
+++ Controller/router.php
@@ -15,7 +15,16 @@ switch(ROUTE[0]) {
     case 'connect'      : AccountController::connect(); break;
     case 'disconnect'   : AccountController::disconnect(); break;
     //case 'signin'       :
-    case 'courses-taken': AccountController::dashboard(); break;
+    case 'dashboard'    :
+
+        switch(ROUTE[1]) {
+
+            case 'courses-taken'    :
+            case 'courses'          :
+            case 'formations'       :
+            case 'accounts'         : AccountController::dashboard(); break;
+            default                 : include_once '../View/Error/404.php'; break;
+        } break;
     default             : include_once '../View/Error/404.php'; break;
 
 }
